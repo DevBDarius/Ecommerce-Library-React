84635e4ddc1bc712c320ba89f3b9a0b75d32fb5f
DevBDarius <d.burinskis@gmail.com>
2021-12-28 20:59:47 +0000

    Discounted section

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -22,7 +22,8 @@ const Book = ({ book }) => {
         <a href="/" className="book__title--link">{book.title}</a>
       </div>
       <div className="book__ratings">
-        {new Array(5).fill(0).map((_, i) => <FontAwesomeIcon icon="star" key={i}/>)}
+        {new Array(Math.floor(book.rating)).fill(0).map((_, i) => <FontAwesomeIcon icon="star" key={i}/>)}
+        {(Math.floor(book.rating) !== book.rating) && <FontAwesomeIcon icon="star-half-alt"/>} {/* If book rating is NOT decimal number, then display a half star */}
       </div>
       <div className="book__price">
         {book.salePrice ?
