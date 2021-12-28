bfed7289c702d9f423a412edb0c2567a02a4502d
DevBDarius <d.burinskis@gmail.com>
2021-12-28 15:41:16 +0000

    Highlights components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import Highlights from './components/Highlights';
 import Landing from './components/Landing';
 import Nav from './components/Nav'; 
+import Highlight from './components/ul/Highlight';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Landing/>
+      <Highlights/>
     </div>
   );
 }
