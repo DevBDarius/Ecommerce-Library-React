f42de6143b3da9d7acd19c8fe52200f989e5297a
DevBDarius <d.burinskis@gmail.com>
2022-01-05 18:44:50 +0000

    Non-dynamic Cart page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Books from './pages/Books';
 import { books } from './data';
 import Bookinfo from './pages/BookInfo';
+import Cart from './pages/Cart';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/books" exact render={() => <Books books={books}/>} />
         <Route path="/books/:id" render={() => <Bookinfo books={books}/>} />
+        <Route path="/cart" render={() => <Cart/>} />
         <Footer/>
       </div>
     </Router>
